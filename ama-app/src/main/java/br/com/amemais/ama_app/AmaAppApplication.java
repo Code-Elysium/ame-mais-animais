e46cac5f2bc728aec62a4053eff7ca73d66c07c2
David Castanheira <david.castanheira@aluno.ifsp.edu.br>
2025-01-23 10:55:20 -0300

    Criação do projeto Spring Boot

---
+++ ama-app/src/main/java/br/com/amemais/ama_app/AmaAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.amemais.ama_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmaAppApplication.class, args);
+	}
+
+}
---
+++ ama-app/src/test/java/br/com/amemais/ama_app/AmaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amemais.ama_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
